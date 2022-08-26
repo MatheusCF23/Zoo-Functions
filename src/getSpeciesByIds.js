b44bd069fea8541fe9cb644e3c9e079e6f01aa7f
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-26 15:50:24 -0300

    Refatorando Desafio 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,14 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return species.filter((elemento) => {
-    return ids.find((nomes) => {
-      return nomes === elemento.id;
-    })
-  })
-}
-
-
-console.log(getSpeciesByIds());
+  return species.filter((elemento) => ids.find((nomes) => nomes === elemento.id));
+};
 
 module.exports = getSpeciesByIds;
