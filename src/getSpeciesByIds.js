553514041837785827cdac9b817ac4b911ead55b
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-26 15:53:18 -0300

    Correção de lint do Desafio 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,6 +3,6 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   return species.filter((elemento) => ids.find((nomes) => nomes === elemento.id));
-};
+}
 
 module.exports = getSpeciesByIds;
