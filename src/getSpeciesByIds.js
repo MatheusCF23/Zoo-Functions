4397489faae3e135ed77e9dfe18bf8b3fa7b987c
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-26 15:44:38 -0300

    Desafio 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  
+function getSpeciesByIds(...ids) {
+  return species.filter((elemento) => {
+    return ids.find((nomes) => {
+      return nomes === elemento.id;
+    })
+  })
 }
+
+
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
