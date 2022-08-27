c190c8158f0d2eabe3da74583a3757c834c9d027
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-27 17:46:33 -0300

    correção lint: Função handlerElephants

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -3,20 +3,20 @@ const handlerElephants = require('../src/handlerElephants');
 describe('Testes da função HandlerElephants', () => {
   it('Retorna a quantidade de elefantes', () => {
     expect(handlerElephants('count')).toBe(4);
-  })
+  });
   it('retorna um array com a relação dos nomes de todos os elefantes', () => {
     expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
-  })
+  });
   it('retorna a média de idade dos elefantes', () => {
     expect(handlerElephants('averageAge')).toEqual(10.5);
-  })
+  });
   it('retorna a localização dos elefantes dentro do Zoológico', () => {
     expect().toEqual();
-  })
+  });
   it('retorna a popularidade dos elefantes', () => {
     expect(handlerElephants('popularity')).toBe(5);
-  })
+  });
   it('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
     expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
-  })
+  });
 });
