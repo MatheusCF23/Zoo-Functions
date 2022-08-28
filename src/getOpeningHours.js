f12b61233a9f688de764818d6c2a664c53b6d06d
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-28 00:49:01 -0300

    Função getOpeningHours

--- src/getOpeningHours.js
+++ src/getOpeningHours.js
@@ -64,4 +64,6 @@ const getOpeningHours = (day, dataHour) => {
   return message;
 };
 
+//console.log(getOpeningHours('Saturday', '10:00-FM'));
+
 module.exports = getOpeningHours;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,35 @@
+const { hours } = require('../data/zoo_data');
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Verifica se o Zoo está aberto na segunda:', () => {
+    expect(getOpeningHours('monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+  it('Verifica se o Zoo está aberto na terça:', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+  it('Verifica se o Zoo está aberto na quarta:', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+  });
+  it('Verifica se o Zoo está aberto na quinta:', () => {
+    expect(getOpeningHours('Thursday', '1:00-PM')).toBe('The zoo is open');
+  });
+  it('Verifica se getOpeningHours é uma função:', () => {
+    expect(typeof getOpeningHours).toBe('function');
+  });
+  it('Verifica se retorna OBJECT', () => {
+    expect(typeof getOpeningHours()).toBe('object');
+  });
+  it('Verifica os dias e horarios:', () => {
+    expect(getOpeningHours()).toEqual(hours);
+  });
+  it('Verifica ERROR - 1', () => {
+    expect(() => {getOpeningHours('Monday', '15:00-PM')}).toThrow('The hour must be between 0 and 12');
+  });
+  it('Verifica ERROR - 2', () => {
+    expect(() => {getOpeningHours('Thu', '10:00-PM')}).toThrow('The day must be valid. Example: Monday');
+  });
+  it('Verifica ERROR - 3', () => {
+    expect(() => {getOpeningHours('Saturday', '10:00-FM')}).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+  });
+});
