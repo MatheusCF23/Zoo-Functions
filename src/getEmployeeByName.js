aabf585d03d4b86288206dac2df342a3695bb2f7
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-28 10:09:32 -0300

    Desafio 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(...employeeName) {
+const { employees: funcionario } = data;
 
+function getEmployeeByName(employeeName) {
+  if(!(employeeName)) {
+    return {};
+  };
+  const retorna = funcionario.reduce((um, dois) => {
+    if(dois.firstName === employeeName || dois.lastName === employeeName) return dois;
+    return um;
+  }, {});
+  return retorna;
 }
+
+console.log(getEmployeeByName('Nelson'));
 
 module.exports = getEmployeeByName;
