793140e2c5eda64567436a65cc8868bae8786e51
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-28 01:45:11 -0300

    ...

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,4 +1,4 @@
-//const { species } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,9 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(...employeeName) {
-  //return species.filter((funcionario) => {
-    //return employeeName.employees === funcionario;
-  //});
+
 }
 
 module.exports = getEmployeeByName;
