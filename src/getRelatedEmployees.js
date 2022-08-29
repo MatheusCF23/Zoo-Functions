c57f2d46822101089c74e25139617cbf54454ab4
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-29 15:34:58 -0300

    Desafio 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
+const { employees: funcionario } = data;
 
 function isManager(id) {
-  // seu código aqui
+    const retornoVerdadeiro = funcionario.some((gerente) => gerente.managers.find ((a) => a === id));
+    return retornoVerdadeiro;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!(isManager(managerId))) {
+    throw  new  Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  return funcionario.reduce((um, dois) => {
+    if (dois.managers.includes(managerId)) {
+      um.push(`${dois.firstName} ${dois.lastName}`);
+    }
+    return um;
+  }, []);
 }
+
 
 module.exports = { isManager, getRelatedEmployees };
