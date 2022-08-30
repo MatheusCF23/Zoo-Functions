39e5fc207bda7cc8c3321d59f6fa6137e40de397
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-30 00:07:03 -0300

    Desafio 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
+const { species } = data
 
+const objeto = () => {
+  const objetoVazio = {};
+  species.forEach((a) => {
+    objetoVazio[a.name] = a.residents.length;
+  });
+  return objetoVazio;
 }
 
+
+
+function countAnimals(animal) {
+  if (animal === undefined) {
+    return objeto();
+  };
+
+  if (Object.keys(animal).length > 1) {
+    return species.find((element) => element.name === animal.specie).residents
+      .filter((sexo) => sexo.sex === animal.sex).length;
+  };
+
+  return species.find((element) => element.name === animal.specie).residents.length;
+}
+
+console.log(countAnimals({ specie: 'elephants', sex: 'female' }));
 module.exports = countAnimals;
