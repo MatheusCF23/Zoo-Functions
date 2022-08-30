704c614bf0e8547a4b6d6691dac9ef223c9613c5
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-30 09:22:43 -0300

    Desafio 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,26 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const objeto = {
+    child: (entrants.filter((person) => person.age < 18)).length,
+    adult: (entrants.filter((person) => person.age >= 18 && person.age < 50)).length,
+    senior: (entrants.filter((person) => person.age >= 50)).length,
+  };
+  return objeto;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+
+  const pessoas = countEntrants(entrants);
+  const valorChild = pessoas.child * prices.child;
+  const valorAdult = pessoas.adult * prices.adult;
+  const valorSenior = pessoas.senior * prices.senior;
+  const result = valorChild + valorAdult + valorSenior
+  return result;
 }
+
+console.log();
 
 module.exports = { calculateEntry, countEntrants };
