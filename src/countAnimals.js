7fae5ce48fa44ebcb1ce059fa26bb9e691470ea2
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-30 00:11:05 -0300

    correção de Lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,6 +1,6 @@
 const data = require('../data/zoo_data');
 
-const { species } = data
+const { species } = data;
 
 const objeto = () => {
   const objetoVazio = {};
@@ -8,19 +8,17 @@ const objeto = () => {
     objetoVazio[a.name] = a.residents.length;
   });
   return objetoVazio;
-}
-
-
+};
 
 function countAnimals(animal) {
   if (animal === undefined) {
     return objeto();
-  };
+  }
 
   if (Object.keys(animal).length > 1) {
     return species.find((element) => element.name === animal.specie).residents
       .filter((sexo) => sexo.sex === animal.sex).length;
-  };
+  }
 
   return species.find((element) => element.name === animal.specie).residents.length;
 }
