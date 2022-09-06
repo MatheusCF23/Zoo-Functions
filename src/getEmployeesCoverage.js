0552aae15c2e7b3ec8b793e6580474942402378a
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-09-06 14:09:38 -0300

    Desafio 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,40 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+function generateEmployeesCoverage() {
+  return employees.map((element) => {
+    const especie = element.responsibleFor
+      .map((element) => species.find((elemento) => elemento.id === element).name);
+    const regioes = especie
+      .map((element) => species.find((elemneto) => elemneto.name === element).location);
+    return {
+      id: element.id,
+      fullName: `${element.firstName} ${element.lastName}`,
+      species: especie,
+      locations: regioes,
+    };
+  });
 }
+
+function getEmployeesCoverage(element) {
+  // seu código aqui
+  const lugar = generateEmployeesCoverage();
+  if (element === undefined) {
+    return lugar;
+  }
+  const { name, id } = element;
+  let worker;
+  if (name !== undefined) {
+    worker = employees.find((element) => element.firstName === name || element.lastName === name);
+  } else if (id !== undefined) {
+    worker = employees.find((element) => element.id === id);
+  }
+  if (worker === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return lugar.find((cover) => cover.id === worker.id);
+}
+
 
 module.exports = getEmployeesCoverage;
